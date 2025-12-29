afb03a225bf113766b05cd371992947b16c526d7
Efterklang <gaojiaxing2004@qq.com>
2025-12-29 14:11:46 +0800

    refactor!: rm lqip for cover image

--- layout/common/article_cover.jsx
+++ layout/common/article_cover.jsx
@@ -5,10 +5,7 @@ module.exports = class extends Component {
     const { page, cover, helper } = this.props;
     const { url_for } = helper;
 
-    const imageSrcset = `${cover}?w=256 256w, ${cover}?w=800 800w, ${cover}?w=1500 1500w, ${cover}?w=2000 2000w, ${cover} 6144w`;
-    const coverLQIP = (
-      <img class="cover-lqip" alt="lqip" src={`${cover}?q=10&blur=25`} />
-    );
+    const imageSrcset = `${cover}?w=800 800w, ${cover}?w=1500 1500w, ${cover}?w=2000 2000w, ${cover} 6144w`;
     const CoverImage = (
       <img
         class="fill"
@@ -25,7 +22,6 @@ module.exports = class extends Component {
       <div class="card-image">
         <a href={url_for(page.link || page.path)} class="image is-7by3">
           {CoverImage}
-          {coverLQIP}
         </a>
       </div>
     );
