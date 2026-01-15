89ac83e7cb76cf9c28d45c75085c1196492e9353
Efterklang <gaojiaxing2004@qq.com>
2026-01-15 22:09:51 +0800

    add swup head plugin to cache assets in <head>

--- layout/plugin/swup.jsx
+++ layout/plugin/swup.jsx
@@ -6,12 +6,22 @@ class Swup extends Component {
       return null;
     }
 
-    const swupScript =
-      'const swup = new Swup({ containers: [".main-content"], });';
+    const swupScript = `
+const swup = new Swup({
+  containers: ["#swup"],
+  cache: true,
+  plugins: [
+    new SwupHeadPlugin({
+      persistTags: true
+    })
+  ]
+});
+`;
     return (
       <Fragment>
         <script src="/js/host/swup/Swup.umd.min.js"></script>
-        <script dangerouslySetInnerHTML={{ __html: swupScript }}> </script>
+        <script src="/js/host/swup/head-plugin.umd.min.js"></script>
+        <script dangerouslySetInnerHTML={{ __html: swupScript }}></script>
       </Fragment>
     );
   }
