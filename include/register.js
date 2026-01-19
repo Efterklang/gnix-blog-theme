9d370146fb739067dd5cc7c4743c24ab9a22686e
Efterklang <gaojiaxing2004@qq.com>
2026-01-19 20:55:59 +0800

    refactor: move hexo module requires from register to index

--- include/register.js
+++
@@ -1,7 +0,0 @@
-module.exports = (hexo) => {
-  require("./hexo/filter")(hexo);
-  require("./hexo/generator")(hexo);
-  require("./hexo/view").init(hexo);
-  require("./hexo/helper")(hexo);
-  require("./hexo/renderer")(hexo);
-};
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,5 @@
-require("../include/register")(hexo);
+require("../include/hexo/filter")(hexo);
+require("../include/hexo/generator")(hexo);
+require("../include/hexo/view").init(hexo);
+require("../include/hexo/helper")(hexo);
+require("../include/hexo/renderer")(hexo);
