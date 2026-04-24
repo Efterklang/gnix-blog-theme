a46c28cdc420150fdaea0c14ba2ce3afcc10daa9
Efterklang <gaojiaxing2004@qq.com>
2026-04-24 18:52:34 +0800

    chore: tweak pagniator style

--- layout/misc/paginator.jsx
+++ layout/misc/paginator.jsx
@@ -57,16 +57,10 @@ module.exports = class extends Component {
     return (
       <nav class="pagination card" aria-label="pagination">
         <a href={getPageUrl(current - 1)} class={`pagination-previous`} style={current > 1 ? {} : { visibility: "hidden" }}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-            <title>go to previous page</title>
-            <path fill="currentColor" d="m4 10l9 9l1.4-1.5L7 10l7.4-7.5L13 1z" />
-          </svg>
+          Prev
         </a>
         <a href={getPageUrl(current + 1)} class={`pagination-next`} style={current < total ? {} : { visibility: "hidden" }}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-            <title>go to next page</title>
-            <path fill="currentColor" d="M7 1L5.6 2.5L13 10l-7.4 7.5L7 19l9-9z" />
-          </svg>
+          Next
         </a>
         <ul class="pagination-list is-hidden-mobile">{pagination(current, total)}</ul>
       </nav>
