3dcb556c2d565e33c3527d7110815426759164ee
Efterklang <gaojiaxing2004@qq.com>
2026-05-05 19:06:49 +0800

    fix: 修复theme selector a11y issue

--- layout/common/theme_selector.jsx
+++ layout/common/theme_selector.jsx
@@ -5,9 +5,20 @@ class ThemeSelector extends Component {
   render() {
     return (
       <div id="theme-selector-popover" popover="auto" tabindex="-1">
-        <div class="theme-selector-list" role="listbox" aria-label="Select theme">
+        <div
+          class="theme-selector-list"
+          role="listbox"
+          aria-label="Select theme"
+        >
           {THEME_OPTIONS.map((theme, index) => (
-            <button class="theme-option" type="submit" data-theme-option={theme.value} data-index={index} onclick={`window.selectThemeOption?.(${index})`}>
+            <button
+              class="theme-option"
+              type="submit"
+              role="option"
+              data-theme-option={theme.value}
+              data-index={index}
+              onclick={`window.selectThemeOption?.(${index})`}
+            >
               {theme.label}
             </button>
           ))}
