67e633dad1f146310c172f3fa639f70fb2142f19
Efterklang <gaojiaxing2004@qq.com>
2026-05-23 02:19:57 +0800

    refactor(feed)!: 移除对rss 2.0的支持，atom去掉content字段，将author字段提到feed顶层

--- include/hexo/feed.js
+++ include/hexo/feed.js
@@ -1,45 +1,16 @@
 const { extname } = require("node:path");
-const { generateRssFeed, generateAtomFeed } = require("feedsmith");
+const { generateAtomFeed } = require("feedsmith");
 const { gravatar, full_url_for, encodeURL, url_for } = require("hexo-util");
 
-const VALID_FEED_TYPES = new Set(["atom", "rss2"]);
-const EXISTING_FEED_LINK_RE = /type=['|"]?application\/(atom|rss)\+xml['|"]?/i;
+const EXISTING_FEED_LINK_RE = /type=['|"]?application\/atom\+xml['|"]?/i;
 const HEAD_RE = /<head>(?!<\/head>).+?<\/head>/s;
-
-function stripControlChars(value) {
-  let sanitizedParts;
-  let start = 0;
-
-  for (let index = 0; index < value.length; index += 1) {
-    const code = value.charCodeAt(index);
-    if (code < 0x20 || code === 0x7f) {
-      sanitizedParts ||= [];
-      if (start < index) {
-        sanitizedParts.push(value.slice(start, index));
-      }
-      start = index + 1;
-    }
-  }
-
-  if (!sanitizedParts) {
-    return value;
-  }
-
-  if (start < value.length) {
-    sanitizedParts.push(value.slice(start));
-  }
-
-  return sanitizedParts.join("");
-}
 
 module.exports = (hexo) => {
   hexo.config.feed = Object.assign(
     {
       enable: true,
-      type: "atom",
-      limit: 20,
+      limit: 0,
       hub: "",
-      content: true,
       content_limit: 140,
       content_limit_delim: "",
       order_by: "-date",
@@ -54,48 +25,10 @@ module.exports = (hexo) => {
     return;
   }
 
-  let type = config.type;
   let path = config.path;
-
-  if (!type || (typeof type !== "string" && !Array.isArray(type))) {
-    type = "atom";
-  }
-
-  if (Array.isArray(type)) {
-    type = [...new Set(type.filter((item) => VALID_FEED_TYPES.has(item)))];
-    if (type.length === 0) {
-      type = "atom";
-    }
-  }
-
-  if (typeof type === "string") {
-    if (!VALID_FEED_TYPES.has(type)) type = "atom";
-  }
-
-  if (!path || typeof path !== typeof type) {
-    if (typeof type === "string") path = type.concat(".xml");
-    else path = type.map((str) => str.concat(".xml"));
-  }
-
-  if (Array.isArray(path)) {
-    if (path.length !== type.length) {
-      if (path.length > type.length) path = path.slice(0, type.length);
-      else if (path.length === 0) path = type.map((str) => str.concat(".xml"));
-      else path.push(type[1].concat(".xml"));
-    }
-    path = path.map((str) => {
-      if (!extname(str)) return str.concat(".xml");
-      return str;
-    });
-  }
-
-  if (typeof path === "string") {
-    if (!extname(path)) path += ".xml";
-  }
-
-  config.type = type;
+  if (!path || typeof path !== "string") path = "atom.xml";
+  if (!extname(path)) path += ".xml";
   config.path = path;
-  const feedEntries = typeof type === "string" ? [{ type, path }] : type.map((feedType, index) => ({ type: feedType, path: path[index] }));
 
   function composePosts(posts, feedConfig) {
     const { limit, order_by } = feedConfig;
@@ -133,8 +66,8 @@ module.exports = (hexo) => {
     };
   }
 
-  function composeFeedLinks(feedUrl, hub, feedType) {
-    const links = [{ href: encodeURL(feedUrl), rel: "self", type: feedType }];
+  function composeFeedLinks(feedUrl, hub) {
+    const links = [{ href: encodeURL(feedUrl), rel: "self" }];
     if (hub) links.push({ href: encodeURL(hub), rel: "hub" });
     return links;
   }
@@ -155,14 +88,6 @@ module.exports = (hexo) => {
       return shortContent;
     }
 
-    return "";
-  }
-
-  function composeItemContent(post, feedConfig) {
-    const { content } = feedConfig;
-    if (content && post.content) {
-      return stripControlChars(post.content);
-    }
     return "";
   }
 
@@ -190,67 +115,28 @@ module.exports = (hexo) => {
     return {
       title: post.title,
       link: encodeURL(full_url_for.call(context, post.permalink)),
+      author: post.author,
       description: composeItemDescription(post, feedConfig),
       published,
       updated: post.updated ? post.updated.toDate() : published,
-      content: composeItemContent(post, feedConfig),
       enclosures: post.image && [{ url: full_url_for.call(context, post.image) }],
       categories: composeItemCategories(post),
     };
   }
 
-  function composeRssItem(feed, item) {
-    return {
-      title: item.title,
-      link: item.link,
-      guid: item.link,
-      description: item.description,
-      pubDate: item.published,
-      authors: [feed.author],
-      content: { encoded: item.content },
-      enclosures: item.enclosures,
-      categories: item.categories,
-    };
-  }
-
   function composeAtomEntry(feed, item) {
-    const entryLinks = [{ href: item.link }, ...(item.enclosures || []).map((enclosure) => ({ href: enclosure.url, rel: "enclosure" }))];
+    const entryLinks = [{ href: item.link, rel: "alternate" }, ...(item.enclosures || []).map((enclosure) => ({ href: enclosure.url, rel: "enclosure" }))];
+    const entryAuthor = item.author && item.author !== feed.author.name ? [{ name: item.author }] : undefined;
     return {
       title: item.title,
       id: item.link,
       links: entryLinks,
       summary: item.description,
-      content: item.content,
       published: item.published,
       updated: item.updated || item.published,
-      authors: feed.author.name && [feed.author],
+      authors: entryAuthor,
       categories: item.categories.map((cat) => ({ term: cat.name, scheme: cat.domain })),
     };
-  }
-
-  function generateRss(feed, items) {
-    const links = composeFeedLinks(feed.feedUrl, feed.hub, "application/rss+xml");
-    const siteUrl = encodeURL(feed.url);
-
-    return generateRssFeed(
-      {
-        title: feed.title,
-        description: feed.description,
-        link: siteUrl,
-        language: feed.language,
-        copyright: feed.copyright,
-        generator: "Hexo",
-        lastBuildDate: feed.updated,
-        image: feed.icon && {
-          url: feed.icon,
-          title: feed.title,
-          link: siteUrl,
-        },
-        atom: { links },
-        items: items.map((item) => composeRssItem(feed, item)),
-      },
-      { lenient: true },
-    );
   }
 
   function generateAtom(feed, items) {
@@ -275,42 +161,23 @@ module.exports = (hexo) => {
     );
   }
 
-  function createFeedGenerator(feedType, feedPath) {
-    return function (locals) {
-      const { config } = this;
-      const { feed: feedConfig } = config;
-      const posts = composePosts(locals.posts, feedConfig);
+  hexo.extend.generator.register("atom", function (locals) {
+    const { config } = this;
+    const { feed: feedConfig } = config;
+    const posts = composePosts(locals.posts, feedConfig);
 
-      if (posts.length <= 0) {
-        return;
-      }
-
-      const feed = composeFeed(feedConfig, config.url, config.email, feedPath, this, posts);
-      const items = posts.toArray().map((post) => composeItem(post, feedConfig, this));
-
-      let data;
-      switch (feedType) {
-        case "rss2":
-          data = generateRss(feed, items);
-          break;
-        default:
-          data = generateAtom(feed, items);
-      }
-
-      return {
-        path: feedPath,
-        data,
-      };
-    };
-  }
-
-  if (typeof type === "string") {
-    hexo.extend.generator.register(type, createFeedGenerator(type, path));
-  } else {
-    for (const entry of feedEntries) {
-      hexo.extend.generator.register(entry.type, createFeedGenerator(entry.type, entry.path));
+    if (posts.length <= 0) {
+      return;
     }
-  }
+
+    const feed = composeFeed(feedConfig, config.url, config.email, feedConfig.path, this, posts);
+    const items = posts.toArray().map((post) => composeItem(post, feedConfig, this));
+
+    return {
+      path: feedConfig.path,
+      data: generateAtom(feed, items),
+    };
+  });
 
   if (typeof config.autodiscovery !== "boolean") config.autodiscovery = true;
 
@@ -320,9 +187,7 @@ module.exports = (hexo) => {
         return data;
       }
 
-      const autodiscoveryTag = feedEntries
-        .map((entry) => `<link rel="alternate" href="${url_for.call(this, entry.path)}" title="${hexo.config.title}" type="application/${entry.type.replace(/2$/, "")}+xml">\n`)
-        .join("");
+      const autodiscoveryTag = `<link rel="alternate" href="${url_for.call(this, config.path)}" title="${hexo.config.title}" type="application/atom+xml">\n`;
 
       return data.replace(HEAD_RE, (str) => str.replace("</head>", `${autodiscoveryTag}</head>`));
     });
